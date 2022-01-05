12d928a7bf00c0567ea5c2c91f70d933dd7bc62c
vidonia <wangjiaqian93@gmail.com>
2022-01-05 09:47:39 +0800

    update

--- src/components/todo_list/List/index.jsx
+++ src/components/todo_list/List/index.jsx
@@ -1,7 +1,13 @@
 
 import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
 import Item from '../Item';
 export default class List extends PureComponent {
+    static propTypes = {
+        todoList: PropTypes.array.isRequired,
+        updateTodo: PropTypes.func.isRequired,
+        deleteTodoItem: PropTypes.func.isRequired,
+    }
     render() {
         const {todoList, updateTodo, deleteTodoItem} = this.props;
         const items = 
